79787b3ef4180e995f66eb3367452e2fd0974b06
iluisbravo <ing.luisalbertobravo@gmail.com>
2018-06-29 12:56:36 -0700

    vista comentarios

--- src/components/movie_review_component/MovieReview.js
+++ src/components/movie_review_component/MovieReview.js
@@ -3,10 +3,17 @@ import './MovieReview.css';
 import MovieReviewTemplate from './MovieReviewTemplate'; 
 
 class MovieReview extends Component {
+  constructor(props){
+    super(props)
+    console.log(this.props)
+    this.state = {
+      idPelicula : this.props.match.params.uid
+    }
+  }
 
   render() {
     return(
-      <MovieReviewTemplate/>
+      <MovieReviewTemplate idPelicula={this.state.idPelicula}/>
     );
   }
 }
--- src/components/movie_review_component/MovieReviewTemplate.js
+++ src/components/movie_review_component/MovieReviewTemplate.js
@@ -1,28 +1,27 @@
 import React, {Component} from 'react'
 import axios from 'axios';
-import {withRouter} from 'react-router-dom';
 
 class MovieReviewTemplate extends Component {
 
   constructor(props){
     super(props);
     
-    console.log(props.match.params.uid)
     this.state = {
-      estrellas: "",
-      comentario: ""
+      idPelicula: this.props.idPelicula,
+      pelicula: {},
+      idCritico : "5b35d5a0f86e7416dd8d3f34",
+      estrellas : 0,
+      comentarios : ""
+      
     }
+
+    console.log(this.state)
   }
 
   onInputChange = (e) => {
-    if (e.target.estrellas == 'estrellas') {
-      this.setState({
-        estrellas: e.target.value
-      })
-    } else if (e.target.comentario == 'comentario'){
-      this.setState({
-        comentario: e.target.value
-      });    
+    switch(e.target.id){
+      case "estrellas" : this.setState({estrellas: e.target.value}) ; break;
+      case "comentario" : this.setState({comentarios: e.target.value}) ; break;
     }
   }
 
@@ -38,44 +37,153 @@ class MovieReviewTemplate extends Component {
     
     } else {
       let json = {
-        estrellas: this.state.estrellas,
-        comentario: this.state.comentario,
+        idPelicula: this.state.idPelicula,
+        idCritico : this.state.idCritico,
+        estrellas: Number(this.state.estrellas),
+        comentarios: this.state.comentarios
       }
+      console.log(json);
 
-      axios.post('https://72d06fd9.ngrok.io/api/v1/calificaciones/', json).then(review => {
+      axios.post('https://booksapiappv1.herokuapp.com/api/v1/calificaciones/', json)
+      .then(review => {
         console.log(review)
-        alert('Tu review ha sido guardado')
+        alert('Tu review ha sido guardado');
+        this.getMovieData();
+        this.clearFormComentario();
+
       }).catch(err => {
-        console.log(err)
+        //console.log(err)
         alert("Lo sentimos, existe un problema")
       })
     }
   }
 
-  render(){
-    console.log(this.state)
-    return (
-      <div className="formulario">
-        <form onSubmit={this.onSubmit}>
+  componentDidMount(){
+    //console.log("Did Mount Movies Template");
+    this.getMovieData();
+  }
+
+  getMovieData(){
+    axios.get(`https://booksapiappv1.herokuapp.com/api/v1/peliculas/${this.state.idPelicula}`)
+    .then(pelicula => {
+      this.setState({
+        pelicula : pelicula.data
+      })
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+
+  updateComentarios(){
+    if(this.state.pelicula._id == undefined){
+      return <div>Cargando</div>
+    }
+    else{
+      let comentarios = this.getComentarios(this.state.pelicula.calificaciones)
+
+      return comentarios ;
+    }
+  }
+
+  updateFormComentario(){
+    if(this.state.pelicula._id == undefined){
+      return <div>Cargando</div>
+    }
+    else{
+      
+      let formComentario = this.getFormComentario();
+      return formComentario;
+    }
+  }
+  
+  clearFormComentario(){
+    document.getElementById("form_comentario").reset();
+  }
+
+  getFormComentario(){
+    let comentarioFormTemplate =   
+    <div class="col-sm-12">            
+        <div className="formulario">
+        <form id="form_comentario" onSubmit={this.onSubmit}>
           <h3>Calificar Película</h3>
           
           <div className="form-group">
             <label>Estrellas</label>
-            <input type="number" name="estrellas" class="form-control" placeholder="calificacion"
+            <input type="number" id="estrellas" name="estrellas" class="form-control" placeholder="calificacion"
               onChange={this.onInputChange}
             />
           </div>
 
           <div className="form-group">
             <label for="exampleFormControlTextarea1">Comentario</label>
-            <textarea name="comentario" class="form-control" rows="3"
+            <textarea id="comentario" name="comentario" class="form-control" rows="3"
             onChange={this.onInputChange}
+            placeholder="Agrega comentario"
             />
           </div>
 
           <button type="submit" class="btn btn-primary">Submit</button>
         </form>
       </div>
+      </div>
+
+      return comentarioFormTemplate;
+  }
+
+  getComentarios(comentarios){
+    
+    let comentariosTemplate = comentarios.map((elem,ix,arr) => {
+      //console.log(elem)
+        return <div class="col-sm-12">
+                <div class="panel panel-white post panel-shadow">
+                    <div class="post-heading">
+                        <div class="pull-left image">
+                            <img src="http://bootdey.com/img/Content/user_1.jpg" class="img-circle avatar" alt="user profile image" />
+                        </div>
+                        <div class="pull-left meta">
+                            <div class="title h5">
+                                <a href="#"><b>{elem.critico}</b></a>
+                                made a post.
+                            </div>
+                            <h6 class="text-muted time">1 minute ago</h6>
+                        </div>
+                    </div> 
+                    <div class="post-description"> 
+                        <p>{elem.comentarios}</p>
+                        <div class="stats">
+                            <a href="#" class="btn btn-default stat-item">
+                                <i class="fa fa-thumbs-up icon"></i>2
+                            </a>
+                            <a href="#" class="btn btn-default stat-item">
+                                <i class="fa fa-thumbs-down icon"></i>12
+                            </a>
+                        </div>
+                    </div>
+                </div>
+             </div>
+             
+    })
+
+    
+
+    return comentariosTemplate;
+  }
+
+  render(){
+    console.log(this.state)
+    return (
+
+        <div class="container">
+            <div class="row">
+              {this.updateFormComentario()}
+            </div>
+            <div class="row">
+              {this.updateComentarios()}
+            </div>
+        </div>
+
+     
     );
   };
 }
